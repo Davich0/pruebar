328f657d807cd9d2340c630d676e177cf844fc7c
Michael Pillaga <michael.pillaga@epn.edu.ec>
2022-07-04 22:28:08 -0500

    agregado prueba 12

--- src/App.java
+++ src/App.java
@@ -1,8 +1,15 @@
+import Prueba12.Prueba12;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("Mi nombre es Michael Pillaga");
         System.out.println("EPN");
+
+        Prueba12.Suma();
+
+        System.out.println("\n");
+        Prueba12.Multi(5, 20);
 
         
     }
---
+++ src/Prueba12/Prueba12.java
@@ -0,0 +1,15 @@
+package Prueba12;
+
+public class Prueba12 {
+    public static void Suma(){
+       int  a=10, b=40 , c=0;
+       c = a + b;
+       System.out.printf("La suma es: %d", c);
+    }
+
+    public static void Multi(int a, int b){
+        int c=0;
+        c = a * b;
+        System.out.printf("La suma es: %d", c);
+     }
+}
